6689ba7a13b6504418f4e64397719b6ef0a96b9a
Tushar Singh <tusharsingh061@gmail.com>
2018-02-12 08:28:25 +0530

    First commit

--- app/src/main/java/in/triumphinfotech/experiment/Models/FacebookLogin.java
+++ app/src/main/java/in/triumphinfotech/experiment/Models/FacebookLogin.java
@@ -6,4 +6,9 @@ package in.triumphinfotech.experiment.Models;
 
 public class FacebookLogin {
     FacebookLogin(){};
+//    final OAuth20Service service = new ServiceBuilder(clientId)
+//            .apiSecret(clientSecret)
+//            .state(secretState)
+//            .callback("http://www.example.com/oauth_callback/")
+//            .build(FacebookApi.instance());
 }
